c22fba5132f5031b873bb73434e5c55c862af21c
Adrian Hernandez <adrianacevhernandez@gmail.com>
2025-10-10 12:12:18 +0800

    Fix CSRF trusted origins and allowed hosts for Railway

--- phishield_project/settings.py
+++ phishield_project/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ------------------------------
 SECRET_KEY = 'django-insecure-6jaqttw(08-+ba(0o+=q#j^tvm&x-97xxpjo$uuzk%$*ad&5qq'
 DEBUG = True
-ALLOWED_HOSTS = ['*']  # ✅ fine for now
+ALLOWED_HOSTS = ['csit327-g8-phishield-production.up.railway.app', '127.0.0.1', 'localhost']
 CSRF_TRUSTED_ORIGINS = [
     'https://csit327-g8-phishield-production.up.railway.app',
 ]
+
 
 # Security settings for production (commented out for development)
 # SECURE_SSL_REDIRECT = True
